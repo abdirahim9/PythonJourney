67ee26fde1c0462cc3676eddf1295374a181edd4
Abdirahim Mohamud <abdirahimmohamud@Abdirahims-MacBook-Air.local>
2025-02-27 15:55:19 +0300

    Calculator script with operations

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - 'for _ in range(3)': Loops 3 times-repeats the questions.
 - 'tries += 1': Counts each loop-tracks attempts.
 - 'print()': Shows  greeting with variables -e.g., "Hello, Abdirahim! At 27, attempt My first Python script-mastery starts here!
+
+# Day 2: Calculator Script
+-'float(input())': Takes number-converts strings to decimals.
+-'Math ops: '+', '-', '*', '/', '//', '%' -compute on 'num1','num2'.
+-'print(f"...)': Formats results-shows all 6 operations cleanly.
+My calc does sums to remainders-math in python is on!
---
+++ calc.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum_result = num1 + num2
+diff_result = num1 - num2
+prod_result = num1 * num2
+div_result = num1 / num2
+int_div_result= num1 // num2  #Integer division
+mod_result= num1 % num2       #Modulus
+print(f"Sum: {num1} + {num2} = {sum_result} ")
+print(f"Difference: {num1} - {num2} = {diff_result} ")
+print(f"Product: {num1} * {num2} = {prod_result} ")
+print(f"Quotient: {num1} / {num2} = {div_result} ")
+print(f"Integer Quotient: {num1} // {num2} = {int_div_result} ")
+print(f"Remainder: {num1} % {num2} = {mod_result} ") 
